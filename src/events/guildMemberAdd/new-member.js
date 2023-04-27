a402ce3aad5e1c07a76b3547439cf119bd0f425b
Dexxieee <130196960+FuzzyFuzzball@users.noreply.github.com>
2023-04-27 23:15:13 +1200

    started memberadd event

--- src/events/guildMemberAdd/new-member.js
+++ src/events/guildMemberAdd/new-member.js
@@ -1,3 +1,9 @@
-// module.exports = {
+module.exports = (member) => {
+    
+        const { user, guild } = member
+        const welcomeChannel = member.guild.channels.cache.get('1084994979316908035')
+        const msg = `Welcome <@${member.id}> to the server.`
 
-// }
+        welcomeChannel.send({ content: msg})
+    
+}
