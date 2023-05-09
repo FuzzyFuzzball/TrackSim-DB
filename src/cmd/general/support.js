90759dea60138a7c4fa86cfa00771f358e207b7f
Dexxieee <130196960+FuzzyFuzzball@users.noreply.github.com>
2023-05-10 10:36:42 +1200

    finished support command

--- src/cmd/general/support.js
+++ src/cmd/general/support.js
@@ -1,6 +1,7 @@
 const { EmbedBuilder } = require('discord.js')
 const { CommandType } = require('wokcommands')
-const { footerlogo, footerlogo, color } = require('../../cfg/embed/embed.json')
+const { footertext, footerlogo, color } = require('../../cfg/embed/embed.json')
+const { supportemail } = require('../../cfg/general/text.json')
 
 module.exports = {
 
@@ -11,5 +12,15 @@ module.exports = {
 
     callback: ({ interaction }) => {
 
+        const reply = new EmbedBuilder()
+        .setColor(color)
+        .setTitle(`Need support for your TrackSim?`)
+        .setDescription(`We have 2 options for support.\n\nOur first option is to open a ticket within the Discord server in the #support channel.\nYou can also email us at ${supportemail} and you can get support through that.`)
+        .setFooter({ text: footertext, iconURL: footerlogo })
+
+        interaction.reply({
+            embeds: [reply],
+            ephemeral: true
+        })
     }
 }
