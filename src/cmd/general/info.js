34bee10a879ebd29e497bfe08b06fe9baec08aee
FuzzyFuzzball <130196960+FuzzyFuzzball@users.noreply.github.com>
2023-05-20 01:21:17 +1200

    started info command

--- src/cmd/general/info.js
+++ src/cmd/general/info.js
@@ -1,10 +1,17 @@
 const { EmbedBuilder } = require('discord.js')
 const { CommandType } = require('wokcommands')
-const { footerlogo, footerlogo, color } = require('../../cfg/embed/embed.json')
+const { footertext, footerlogo, color } = require('../../cfg/embed/embed.json')
 
 module.exports = {
+    description: `Gives information about TrackSim.`,
+    type: CommandType.SLASH,
+    testOnly: true,
+    guildOnly: true,
 
     callback: ({ interaction }) => {
-
+        const reply = new EmbedBuilder()
+        .setColor(color)
+        .setTitle(`TrackSim Information`)
+        .setDescription(`TrackSim was founded in 2023, owned by Omnibyte Technologies. It was built because of the close down of Navio.`)
     }
 }
